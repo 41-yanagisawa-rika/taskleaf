64c5e2c99f86e38f10c17cef9858285eeafa20ab
41_yanagisawa_rika <contra.0039@gmail.com>
2023-03-28 10:08:24 +0900

    taskleaf0327

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_26_142645) do
+ActiveRecord::Schema.define(version: 2023_03_27_122050) do
 
 # Could not dump table "tasks" because of following StandardError
 #   Unknown type 'strings' for column 'name'
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2023_03_26_142645) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
